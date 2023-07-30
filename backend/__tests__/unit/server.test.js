eff09ce4ef9ce5726fc2eff863e37b70c39995e0
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2023-07-31 10:52:00 +1200

    Test for server.js done.

---
+++ backend/__tests__/unit/server.test.js
@@ -0,0 +1,30 @@
+/*
+    Importing the Server function into the test file
+    Jest automatically mocks the function by default
+*/
+const Server = require('../../server');
+
+// Importing https tp use with createServer
+const https = require('https');
+
+// Explicitly mocking the https module with createServer and listen functions attached
+jest.mock('https', () => ({
+    createServer: jest.fn(() => ({ listen: jest.fn() })),
+    })
+);
+
+// Grouping Server tests together with describe if more than one exist
+describe('Server', () => {
+    /* 
+        Test states what it does in a string
+        Arranges the test 
+    */
+    test('should create server on port 8080', () => {
+
+        // Act to create a new Server instantiation of the class and call the method
+        const server = new Server().startServer();
+
+        // Assert the https.createServer mock is called
+        expect(https.createServer).toBeCalled();
+    });
+});
