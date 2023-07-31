6dabc587ed07f6ad49722e8e11eda283de096eb4
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2023-08-01 10:30:29 +1200

    Variable to house the new server.

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,16 @@ const https = require('https');
 
 // Jest mocks prefer objects and constructors, we need to use classes
 class Server {
+    
     // Constructor method for starting the server
     startServer() {
         // Server creation method
-            https.createServer().listen(8080, console.log('server'));
+            this.run = https.createServer().listen(5000, console.log(`Server running on port 5000`));
         }
     }
+
+// Runs the server
+const run = new Server();
 
 // Exporting the module for use in tests
 module.exports = Server;
