b86ab99c548457f4893aee37e52581dae7cde3e3
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2023-08-01 13:29:17 +1200

    Commeted the file.

--- backend/__tests__/unit/app.test.js
+++ backend/__tests__/unit/app.test.js
@@ -1,9 +1,19 @@
+// importing request from supertest for testing routes
 const request = require('supertest');
+
+// Importing app to be tested
 const app = require('../../app/app');
 
-describe("Test the root path", () => {
-    test("It should response the GET method", async () => {
+// Describing the grouped tests
+describe("Testing routes", () => {
+
+    // Describing the current test and Arranging it
+    test("the GET method should return", async () => {
+
+        // Act to be tested
         const response = await request(app).get("/");
+
+        // Assert and repsonse 
         expect(response.statusCode).toBe(200)
     });
 });
