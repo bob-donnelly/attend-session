58185ddbfec33efd33e595b757d4d12c6e2e3891
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2023-08-01 13:33:06 +1200

    Commeted the file.

--- backend/app/app.js
+++ backend/app/app.js
@@ -1,9 +1,14 @@
+// Impoting express library to be used
 const express = require('express');
+
+// Creating the app
 const app = express();
 
+// Using the app to serve the routes 
 app.get("/", (req, res) => {
     res.status(200).send("Hello World!");
     console.log('test')
 });
 
+// Exporting the app module
 module.exports = app;
