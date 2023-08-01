2f1d58d107b03452a329bf2a9bda09a499255e35
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2023-08-02 11:33:37 +1200

    Refactored Server to be an anonymous function instead of a class.

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const https = require('https');
 // Imports the app module so we can use it within the server
 const app = require('./app/app');
 
+const { connectDB, disconnectDB } = require('./database/connection');
+
 // imports dotenv module
 const dotenv = require('dotenv');
 
@@ -11,19 +13,21 @@ const dotenv = require('dotenv');
 dotenv.config()
 const port = process.env.port || 7000;
 
-// Jest mocks prefer objects and constructors, we need to use classes
-class Server {
-    
-    // Method for starting the server
-    startServer() {
+// Function for starting the server
+Server = () => {
+    // Private Server creation method 
+    this.run = https.createServer(app).listen(port, console.log(`Server running on port 6000`));
 
-        // Server creation method
-            this.run = https.createServer(app).listen(port, console.log(`Server running on port 6000`));
-        }
-    }
+    // Database connection function
+    connectDB();
+
+    // Database disconnect function
+
+    disconnectDB();
+}
+
 // Runs the server
-const run = new Server()
-run.startServer();
+Server();
 
 // Exporting the module for use in tests
 module.exports = Server;
