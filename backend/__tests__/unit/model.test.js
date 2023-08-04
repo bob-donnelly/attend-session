cdaa1d468eb7a72fcbea8075f7fad67c7a098440
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2023-08-04 15:24:24 +1200

    Changed the second test to have the spread operator iterate over userData to avoid needing a static _id field.

--- backend/__tests__/unit/model.test.js
+++ backend/__tests__/unit/model.test.js
@@ -58,8 +58,8 @@ describe('userModel tests', () => {
     test('should save user and ignore extra fields', async() => {
 
         // Passing the userData with an extra field added to test if it will be ignored
-        const userWithWrongField = Users(userData, {
-            _id: '64cc4fb93d9586d075099028', 
+        const userWithWrongField = Users({
+            ...userData,
             name: "invalid"
         });
 
