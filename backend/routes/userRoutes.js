c6b2a382dc224de75a83d07ed1f57f763c44aa1e
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2023-08-21 12:21:40 +1200

    Importing the controllers into routes instead of hardcoded app route examples.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,28 +1,21 @@
 // Importing express router to be used
 const router = require('express').Router();
 
-// // Controllers that we need to create routes
-// const {
-//     getAllUsers,
-//     getUserById
-// } = require('../controller/userControllers');
+// Controllers that we need to create routes
+const {
+    getAllUsers,
+    getUserById
+} = require('../controller/userControllers');
 
-router.get("/", (req, res) => {
-    res.status(200).send("Hello World!");
-});
+// Routes for admins
+router
+    .route('/admin')
+    .get(getAllUsers)
 
-router.get("/admin", (req, res) => {
-    res.status(200).send("Hello Admin!");
-});
-// // Routes for admins
-// router
-//     .route('/admin')
-//     .get(getAllUsers)
-
-// // Routes for users
-// router
-//     .route('/account')
-//     .get(getUserById)
+// Routes for users
+router
+    .route('/account')
+    .get(getUserById)
 
 // Exports all routers as router
 module.exports = router;
