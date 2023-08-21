87c5479019ddc1f53e7d20100283cfe768078496
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2023-08-21 12:44:09 +1200

    Added cors to my express backend.

--- backend/app/app.js
+++ backend/app/app.js
@@ -1,5 +1,6 @@
 // Impoting express library to be used
 const express = require('express');
+const cors = require('cors');
 
 // Importing routes from router module
 const userRoutes = require('../routes/userRoutes')
@@ -8,7 +9,10 @@ const userRoutes = require('../routes/userRoutes')
 const app = express();
 
 // Using json middleware from express
-app.use(express.json())
+app.use(express.json());
+
+// Using cross origin requests to allow React to connect
+app.use(cors());
 
 // Using the app to serve the routes 
 app.use("/user", userRoutes);
