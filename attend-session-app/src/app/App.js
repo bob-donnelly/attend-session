e8355f2ed6c9cd06348a02a6e813ef69651140b7
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2023-08-21 12:56:14 +1200

    Example of fetching all users from the database.

--- attend-session-app/src/app/App.js
+++ attend-session-app/src/app/App.js
@@ -1,21 +1,31 @@
-import './App.css';
+import React, {useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([])
+
+  const fetchUserData = () => {
+    fetch("http://127.0.0.1:6012/user/admin")
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setUsers(data)
+      })
+  }
+  
+  useEffect(() => {
+    fetchUserData()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {users.length > 0 && (
+        <ul>
+          {users.map(user => (
+            <li key={user.id}>{user.userName}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
