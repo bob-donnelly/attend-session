89a1eff47d8ca86f2093163ae4d61520c1a1a02e
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2023-08-29 17:02:12 +1200

    Added the routing function to the App for the user profile and admin panel.

--- attend-session-app/src/app/App.js
+++ attend-session-app/src/app/App.js
@@ -1,33 +1,29 @@
-import React, {useEffect, useState } from 'react';
+//Importing the Browserrouter from react router dom in three parts to link the front end together
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+// Importing views from the views folder
+import UserProfile from '../view/userProfile'
+import AdminPanel from '../view/adminPanel'
+
+// Application function that will house all of the page components
 function App() {
-  const [users, setUsers] = useState([])
 
-  const fetchUserData = () => {
-    fetch("http://127.0.0.1:6012/user/admin")
-      .then(response => {
-        return response.json()
-      })
-      .then(data => {
-        setUsers(data)
-      })
-  }
-  
-  useEffect(() => {
-    fetchUserData()
-  }, [])
-
-  return (
-    <div>
-      {users.length > 0 && (
-        <ul>
-          {users.map(user => (
-            <li key={user.id}>{user.userName}</li>
-          ))}
-        </ul>
-      )}
-    </div>
+  // Returns the router which will house all of the routes
+  return(
+    <Router>
+      <main> 
+      {/* 
+          Routes from react-router-dom which declare a route, 
+          the exact path and the element which is the component for that route 
+      */}
+          <Routes>
+          <Route exact path="/profile" element={<UserProfile />}/>
+          <Route exact path="/admin" element={<AdminPanel />}/>
+          </Routes>
+      </main>
+    </Router>
   );
-}
+};
 
+// Export default is used if there is only one function in the file
 export default App;
