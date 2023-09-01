69a0247bb8c8fd683217ee425baf0f913627e6e5
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2023-09-01 13:33:56 +1200

    Added tests for the first part of the admin panel user table.

---
+++ attend-session-app/src/__tests__/unit/adminPanel.test.js
@@ -0,0 +1,36 @@
+// Importing jest-dom for testing React
+import '@testing-library/jest-dom';
+
+// Importing render for testing React
+import { render, screen } from '@testing-library/react';
+
+// Importing User Table from admin components
+import UserTable from '../../view/adminPanel';
+
+// Test suite for admin panel view
+describe('admin panel view', () => {
+
+    // Test for the User Table
+    test('User table header test', () => {
+
+        // Render the User Table
+        render(<UserTable />);
+
+        // Asserting the expected results
+        expect(screen.getByText('Username')).toBeInTheDocument();
+        expect(screen.getByText('Email')).toBeInTheDocument();
+        expect(screen.getByText('First Name')).toBeInTheDocument();
+        expect(screen.getByText('Last Name')).toBeInTheDocument();
+        expect(screen.getByText('Group Name')).toBeInTheDocument();
+    });
+
+    // Test for the User Table render test
+    test('Should be able to get table by role', () => {
+
+        // TRendering the table
+        render(<UserTable />);
+
+        // Asserting the expected results
+        expect(screen.getByRole('table')).toBeInTheDocument();
+    });
+});
