f46957c2b4cf86b206aad2326098c15c2661aae0
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2023-09-12 14:47:49 +1200

    Refactored my test to check against the database not a hardcoded number.

--- attend-session-app/src/__tests__/unit/adminTableComponent.test.js
+++ attend-session-app/src/__tests__/unit/adminTableComponent.test.js
@@ -7,6 +7,21 @@ import { renderHook, waitFor } from '@testing-library/react';
 // Importing components for testing
 import useGetAllUsers from '../../components/adminTableComponent';
 
+// Fetch request to check the custom hook is not changing the base data
+const getAll = async () => {
+
+    // Await the users data from the admin route
+    const response = await fetch("http://127.0.0.1:6012/user/admin");
+
+    // If the response is ok await the response as data then return the data
+    if(response.ok) {
+        const data = await response.json();
+        return data;
+    } else {
+        response.json({message: "Users not found"});
+    };
+};
+
 // Test suite for the admin components
 describe('Tests for all user components', () => {
     
@@ -16,8 +31,10 @@ describe('Tests for all user components', () => {
             // Defining result as equal to the custom hook that fetches all users
             const { result } = renderHook(() => useGetAllUsers());
 
-            // Asserting that the total number of users is 461
+            const users = await getAll();
+
+            // Asserting that the total number of users is equal to the number in the database
             await waitFor(() => 
-            expect(result.current.length).toEqual(461));
+            expect(result.current.length).toEqual(users.length));
     });
 });
