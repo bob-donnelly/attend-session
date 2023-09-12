42ba9caf1e4bdfa33a9684b6d5f18bc934e2fea9
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2023-09-12 13:52:54 +1200

    Created the registration controller.

--- backend/controller/userControllers.js
+++ backend/controller/userControllers.js
@@ -12,7 +12,7 @@ const getAllUsers = async (req, res) => {
         res.json(user);
     } else {
         res.status(404).json({ message: 'Users not found' });
-    }
+    };
 };
 
 // function to get one user from MongoDB by id
@@ -26,11 +26,66 @@ const getUserById = async (req, res) => {
         res.json(user);
     } else {
         res.status(404).json({ message: 'User not found' });
-    }
+    };
 };
 
-// Exporting functions to be used in routes and tests
+const registration = async (req, res) => {
+
+    /* 
+        userName, email, password groupName, firstName, lastName, and admin 
+        are in the request body to be sent to the database
+    */
+
+    const { userName, email, password, groupName, firstName, lastName, admin } = req.body
+
+    // Function checks that the user email does not already exist
+
+    const userExists = await Users.findOne({ email });
+
+    // An if statement that returns a message if user exists already
+
+    if(userExists) {
+        res.status(404).json({message: "User already exists."});
+    } else {
+
+        /* 
+            User is created with a mongoose create function and it waits for the user model for the template 
+            then if the arguments are correct i.e., fulfil the requirments of the schema it will create the new user   
+        */
+        const user = await Users.create({ 
+            userName,
+            email,
+            password,
+            groupName, 
+            firstName, 
+            lastName, 
+            admin
+        });
+
+        /* 
+            If User is created the user will respond with the user data if it is not created 
+            it will respond with server error
+        */
+        if(user) {
+            res.json({
+                _id: user._id,
+                userName: user.userName,
+                email: user.email,
+                password: user.password,
+                groupName: user.groupName,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                admin: user.admin
+            });
+        } else {
+            res.status(404).json({ message: "Server error." });
+        };
+    };
+};
+
+// Exporting functions to be used in routes
 module.exports = {
     getAllUsers,
-    getUserById
+    getUserById,
+    registration
 }
