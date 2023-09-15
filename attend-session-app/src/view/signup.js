8fa21ce01a855c05684e2504ac0e1d5cb575ddce
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2023-09-15 16:14:54 +1200

    Changed the div to form and passed in functions to create a user.

--- attend-session-app/src/view/signup.js
+++ attend-session-app/src/view/signup.js
@@ -4,33 +4,79 @@ import useRegistration from '../components/signupComponent';
 // Registration view 
 const Signup = () => {
 
+    // Importing the functions needed from the custom hook
+    const { values, errors, handleChange, handleSubmit } = useRegistration();
+
+    // Returning the signup form with the ability to submit the form with values
     return (
         <div>
             <h1>Signup</h1>
-            <div className="form">
+                <form onSubmit={handleSubmit}>
                 <p>Username</p>
-                <input type="text" placeholder="Username"/>
+                    <input 
+                        name="userName" 
+                        type="text" 
+                        placeholder="Username" 
+                        value={values.userName} 
+                        onChange={handleChange}
+                    />
                 <p>Email</p>
-                <input type="email" placeholder="Email"/>
+                    <input 
+                        name="email" 
+                        type="email" 
+                        placeholder="Email" 
+                        value={values.email} 
+                        onChange={handleChange}
+                    />
                 <p>Password</p>
-                <input type="password" placeholder="Password"/>
+                    <input 
+                        name="password" 
+                        type="password" 
+                        placeholder="Password" 
+                        value={values.password} 
+                        onChange={handleChange}
+                    />
                 <p>Confirm Password</p>
-                <input type="password" placeholder="Confirm Password"/>
+                    <input 
+                        name="confirmPassword" 
+                        type="password" 
+                        placeholder="Confirm Password" 
+                        value={values.confirmPassword} 
+                        onChange={handleChange}
+                    />
+                <p>{errors}</p>   
                 <p>Group Name</p>
-                <input type="text" placeholder="Group Name"/>
+                    <input 
+                        name="groupName" 
+                        type="text" 
+                        placeholder="Group Name" 
+                        value={values.groupName} 
+                        onChange={handleChange}
+                    />
                 <p>First Name</p>
-                <input type="text" placeholder="First Name"/>
+                    <input 
+                        name="firstName" 
+                        type="text" 
+                        placeholder="First Name" 
+                        value={values.firstName} 
+                        onChange={handleChange}
+                    />
                 <p>Last Name</p>
-                <input type="text" placeholder="Last Name"/>
+                    <input 
+                        name="lastName" 
+                        type="text" 
+                        placeholder="Last Name" 
+                        value={values.lastName} 
+                        onChange={handleChange}
+                    />
                 <br></br>
                 <br></br>
-                <button>Signup</button>
-
+                <button type="submit">Signup</button>
+                </form>
                 <p>
                     Already have an account? 
                     <a href="/login"> Login</a>
                 </p>
-            </div>
         </div>
     )
 };
