f0befcb056dfa875d139e6347e1ec408defb0478
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2023-09-19 13:16:12 +1200

    Added a undefined check to token.

--- attend-session-app/src/components/auth.js
+++ attend-session-app/src/components/auth.js
@@ -1,13 +1,25 @@
+// Declaring a custom hook
 const useToken = () => {
+    
+    // Function to get the token set by the server when a user is logged in
     const getToken = () => {
         const tokenString = sessionStorage.getItem('token');
         const token = JSON.parse(tokenString);
-        return token
-    };
 
+            // Checking if the token doesn't exist and setting it to null or return the token
+            if(token === undefined) {
+                return null;
+            }
+            else {
+                return token
+            };
+        };
+
+    // Returning the getToken function
     return {
         getToken
     };
 };
 
+// Exporting the custom hook as default
 export default useToken;
