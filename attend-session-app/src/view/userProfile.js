7c8321b4c689d0061cf1e6a2ab928d94d5d09f22
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2023-09-22 09:35:10 +1200

    Added calls to the profile component to update a single or multiple fields.

--- attend-session-app/src/view/userProfile.js
+++ attend-session-app/src/view/userProfile.js
@@ -1,36 +1,83 @@
 // Import the fetch logic custom hook from the user components
 import useGetOneUser from '../components/userFetchByIdComponent';
 
+import useUpdateProfile from '../components/updateProfileComponent';
+
 // UserProfile view component
 const UserProfile = () => {
 
     // Defining user as equal to the custom hook that fetches the user data from an id
     const user = useGetOneUser();
     
+    const { values, errors, handleChange, handleSubmit } = useUpdateProfile();
     /* 
         Shows the values of the user next to a text explainer of what the value is
         Also has input fields for the user to change their details which is submitted by the change details button
     */
     return(
         <div>
-            <p>User Name: {user.userName}</p>
-                <input type="text" placeholder="Change Username"/>
-            <p>Email: {user.email}</p>
-                <input type="text" placeholder="Change Email"/>
-            <p>Password</p>
-                <input type="text" placeholder="Change Password"/>
-            <br/> 
-            <br/>
-                <input type="text" placeholder="Confirm Password"/>
-            <p>Group Name: {user.groupName}</p>
-                <input type="text" placeholder="Change Group"/>
-            <p>First Name: {user.firstName}</p>
-                <input type="text" placeholder="Change First Name"/>
-            <p>Last Name: {user.lastName}</p>
-            <input type="text" placeholder="Change Last Name"/>
-            <br/> 
-            <br/>
-            <button>Change details</button>
+            <form onSubmit={handleSubmit}>
+                <p>{errors}</p>  
+                <p>User Name: {user.userName}</p>
+                    <input 
+                    name="userName"
+                    type="text" 
+                    placeholder="Change Username"
+                    value={values.userName} 
+                    onChange={handleChange}
+                            />
+                <p>Email: {user.email}</p>
+                    <input 
+                    name="email"
+                    type="text" 
+                    placeholder="Change Email"
+                    value={values.email} 
+                    onChange={handleChange}/>
+                <p>Password</p>
+                    <input 
+                    name="password"
+                    type="password" 
+                    placeholder="Change Password"
+                    value={values.password} 
+                    onChange={handleChange}
+                    />
+                <br/> 
+                <br/>
+                    <input 
+                    name="confirmPassword"
+                    type="password" 
+                    placeholder="Confirm Password"
+                    value={values.confirmPassword} 
+                    onChange={handleChange}
+                    />
+                <p>Group Name: {user.groupName}</p>
+                    <input 
+                    name="groupName"
+                    type="text" 
+                    placeholder="Change Group"
+                    value={values.groupName} 
+                    onChange={handleChange}
+                        />
+                <p>First Name: {user.firstName}</p>
+                    <input 
+                    name="firstName"
+                    type="text" 
+                    placeholder="Change First Name"
+                    value={values.firstName} 
+                    onChange={handleChange}
+                    />
+                <p>Last Name: {user.lastName}</p>
+                    <input 
+                    name="lastName"
+                    type="text" 
+                    placeholder="Change Last Name"
+                    value={values.lastName} 
+                    onChange={handleChange}
+                    />
+                <br/> 
+                <br/>
+                <button type="submit">Change details</button>
+            </form>
         </div>
         );
 };
