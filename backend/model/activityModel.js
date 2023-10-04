6f2f19b82402fd2dc9d5b82b388247cb16494f69
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2023-10-04 16:27:16 +1300

    Created the activity model.

--- backend/model/activityModel.js
+++ backend/model/activityModel.js
@@ -3,6 +3,54 @@ const mongoose = require('mongoose');
 const { Schema } = require('mongoose');
 
 const activitiesSchema = new Schema({
+    weeklyActivityName: {
+        type: String,
+        required: true,
+    },
+        locations: [{
+            _id: false,
+            hallName: {
+            type: String,
+            required: true
+            },
+            address: {
+                type: String,
+                required: true
+            },
+            city: {
+                type: String,
+                required: true
+            },
+            region: {
+                type: String,
+                required: true
+            },
+        }],
+            days: [{
+                type: String,
+                required: true
+            }],
+            times: [{
+                _id: false,
+                startTime: {
+                    type: String, 
+                    required: true
+                },
+            endTime: {
+                type: String,
+                required: true
+            }
+        }],
+    createdBy: {
+        type: mongoose.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    attendedBy: [{
+        type: mongoose.ObjectId,
+        ref: 'User',
+        required: false
+    }]
 });
 
 module.exports = mongoose.model('Activities', activitiesSchema);
